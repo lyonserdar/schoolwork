fd1b13ff00f81c3978f0692bd3d8a19e944d1732
Serdar Lyon <lyonserdar@gmail.com>
2021-02-18 15:47:56 -0700

    ML Formatting

--- Intro to Machine Learning (datacamp)/dataset_iris.py
+++ Intro to Machine Learning (datacamp)/dataset_iris.py
@@ -10,6 +10,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=10, stratify=y
 )
 
+
 def main():
     print(iris.DESCR)
     print(iris.keys())
--- Intro to Machine Learning (datacamp)/exploratory_data_analysis.py
+++ Intro to Machine Learning (datacamp)/exploratory_data_analysis.py
@@ -10,16 +10,16 @@ print(type(iris))
 print(iris.keys())
 print(iris.target_names)
 
-X = iris['data']
-y = iris['target']
+X = iris["data"]
+y = iris["target"]
 
-df = pd.DataFrame(X, columns=iris['feature_names'])
+df = pd.DataFrame(X, columns=iris["feature_names"])
 
 print(df.head())
 print(df.info())
 print(df.describe())
 
-pd.plotting.scatter_matrix(df, c=y, figsize=[8,8], s=150, marker='D')
+pd.plotting.scatter_matrix(df, c=y, figsize=[8, 8], s=150, marker="D")
 plt.show()
 
 # If dataset is binary Seaborn's countplot is more useful.
