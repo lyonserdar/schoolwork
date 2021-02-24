54e01951341c50941f509e0a8f027430d58cc0a6
Serdar Lyon <lyonserdar@gmail.com>
2021-02-24 14:46:35 -0700

    ECE 3710 - Homework 6 started

---
+++ Probability and Statistics/homework_m06.py
@@ -0,0 +1,75 @@
+__class__ = "ECE 3710"
+__project__ = "M06 Homework"
+__author__ = "Ali Serdar Aydogdu"
+__email__ = "lyonserdar@gmail.com"
+__date__ = "2/23/2021"
+__divider__ = "------------------------------------------------------------------------"
+
+
+### 1. Sec 4.2, #4 (15 points)
+print(__divider__)
+print("1. Sec 4.2, #4 (15 points)")
+print(__divider__)
+"""
+At a certain airport, 75% of the flights arrive on time. A sample of 10 flights is
+studied.Hint: Estimate p. Then use a Binomial pmf or CDF function with parameters n=10
+and p.
+"""
+# Setup
+
+# a. Find the probability that all 10 of the flights were on time.
+
+# b. Find the probability that exactly eight of the flights were on time.
+
+# c. Find the probability that eight or more of the flights were on time.
+
+
+### 2. Sec 4.3, #8 (15 points)
+print(__divider__)
+print("1. Sec 4.3, #8 (15 points)")
+print(__divider__)
+"""
+The number of cars arriving at a given intersection follows a Poisson distribution with
+a mean rate of 4 per second.
+"""
+# Setup
+
+# a. What is the probability that 3 cars arrive in a given second (i.e. 1 second
+#    interval)? 
+
+# b. What is the probability that 8 cars arrive in a period of three seconds?Hint: The
+#    Poisson parameter will be different based on the time interval of interest. So, 𝜆in
+#    1 second, 2𝜆 in 2 seconds, etc.
+
+# c. What is the probability that more than 3 cars arrive in a period of two
+#    seconds?Hint: Use the fundamental rule that P(A)= 1 - P(A^c)
+
+
+### 3. Sec 4.3, #10 (10 points)
+print(__divider__)
+print("3. Sec 4.3, #10 (10 points)")
+print(__divider__)
+"""
+A chemist wishes to estimate the concentration of particles in a certain suspension. She
+withdraws 3 mL of the suspension and counts 48 particles. 
+"""
+# Setup
+
+# a. Estimate the concentration in particles per mL.  
+
+# b. Find the uncertainty in the estimate.
+
+
+### Extra Credit (10 points)
+print(__divider__)
+print("Extra Credit (10 points)")
+print(__divider__)
+
+# a. Search online for an example of a Poisson process and give the reference. (Hint:
+#    Use Google Scholar or similar search tool for academic articles).
+
+# b. Describe the experiment or process from your reference and explain what makes it a
+#    Poisson process.
+
+# c. Using the Poisson parameters given in the reference, pose your own question and
+#    make an appropriate Poisson calculation.
