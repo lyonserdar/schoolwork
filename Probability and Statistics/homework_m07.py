0e5763f9d97eaf056df0e79948857408472ea502
Serdar Lyon <lyonserdar@gmail.com>
2021-03-15 12:25:14 -0600

    ECE 3710 - Homework 7 completed

--- Probability and Statistics/homework_m07.py
+++ Probability and Statistics/homework_m07.py
@@ -14,7 +14,9 @@ __divider__ = "-----------------------------------------------------------------
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from scipy.stats import binom, poisson
+from scipy.stats import norm, expon, probplot
+from sympy import symbols, Eq, solve
+from math import e
 
 ### 1. Sec 4.5, #4 (15 points)
 print(__divider__)
@@ -27,22 +29,25 @@ statistical functions, ensure you know if they input variance or standard deviat
 X ∼ N(2, 9)
 """
 # Setup
-p = 0.75
-n = 10
-fig, ax = plt.subplots(1, 1)
-mean, var, skew, kurt = binom.stats(n, p, moments="mvsk")
-x = np.arange(binom.ppf(0.01, n, p), binom.ppf(0.99, n, p))
-ax.plot(x, binom.pmf(x, n, p), "bo", ms=8, label="binom pmf")
-ax.vlines(x, 0, binom.pmf(x, n, p), colors="b", lw=5, alpha=0.5)
+mean = 2
+variance = 9
+std = np.sqrt(variance)
 
 # a. P(X≥2)
+print(1 - norm(mean, std).cdf(2))
 
 # b. P(1≤X<7)
+print(norm(mean, std).cdf(7) - norm(mean, std).cdf(1))
 
 # c. P(−2.5<X<−1)
+print(norm(mean, std).cdf(-1) - norm(mean, std).cdf(-2.5))
 
 # d. P(−3≤X−2< 3) Hint: X-2 represents a mean shift.
+print(abs(norm(mean - 2, std).cdf(-3) - norm(mean - 2, std).cdf(3)))
 
+x = np.linspace(mean - 3 * std, mean + 3 * std, 100)
+plt.plot(x, norm.pdf(x, mean, std))
+plt.plot(x, norm.cdf(x, mean, std))
 plt.show()
 
 ### 2. Sec 4.5, #12 (15 points)
@@ -60,17 +65,23 @@ simple equations with the known strength values and two unknowns (μ and σ).  U
 algebra to solve for the mean (μ) and standard deviation (σ).
 """
 # Setup
-mu = 4
-fig, ax = plt.subplots(1, 1)
-mean, var, skew, kurt = poisson.stats(n, p, moments="mvsk")
-x = np.arange(poisson.ppf(0.01, mu), poisson.ppf(0.99, mu))
-ax.plot(x, poisson.pmf(x, mu), "bo", ms=8, label="poisson pmf")
-ax.vlines(x, 0, poisson.pmf(x, mu), colors="b", lw=5, alpha=0.5)
+z1 = norm.ppf(0.10, loc=0, scale=1)
+z2 = norm.ppf(0.95, loc=0, scale=1)
+mean = 0
+std = 0
 
 # a. Find the mean and standard deviation of the strengths.
+std = (19.76 - 18.3) / (z2 - z1)
+print("std", std)
+mean = 19.76 - z2 * std
+print("mean", mean)
 
 # b. What proportion of the bolts meet the strength specification?
+print(1 - norm(mean, std).cdf(18))
 
+x = np.linspace(mean - 3 * std, mean + 3 * std, 100)
+plt.plot(x, norm.pdf(x, mean, std))
+plt.plot(x, norm.cdf(x, mean, std))
 plt.show()
 
 ### 3. Sec 4.5, #16 (15 points)
@@ -82,22 +93,26 @@ The amount of paint required to paint a surface with an area of 50 m^2 is normal
 distributed with mean 6 L and standard deviation 0.3 L.
 """
 # Setup
-mu = 48 / 3
-fig, ax = plt.subplots(1, 1)
-mean, var, skew, kurt = poisson.stats(n, p, moments="mvsk")
-x = np.arange(poisson.ppf(0.01, mu), poisson.ppf(0.99, mu))
-ax.plot(x, poisson.pmf(x, mu), "bo", ms=8, label="poisson pmf")
-ax.vlines(x, 0, poisson.pmf(x, mu), colors="b", lw=5, alpha=0.5)
+mean = 6
+std = 0.3
 
 # a. If 6.2 L of paint are available, what is the probability that the entire surface
 #    can be painted?
+print(norm(mean, std).cdf(6.2))
 
 # b. How much paint is needed so that the probability is 0.9 that the entire surface can
 #    be painted?
+z = norm.ppf(0.9, loc=mean, scale=std)
+print(z)
 
 # c. What must the standard deviation be so that the probability is 0.9 that 6.2 L of
 #    paint will be sufficient to paint the entire surface?
+new_std = (6.2 - mean) / z
+print(new_std)
 
+x = np.linspace(norm.ppf(0.01, mean, std), norm.ppf(0.99, mean, std), 100)
+plt.plot(x, norm.pdf(x, mean, std))
+plt.plot(x, norm.cdf(x, mean, std))
 plt.show()
 
 ### 4. Sec 4.7, #2 (20 points)
@@ -109,26 +124,35 @@ The time between requests to a web server is exponentially distributed with mean
 seconds.
 """
 # Setup
-mu = 48 / 3
-fig, ax = plt.subplots(1, 1)
-mean, var, skew, kurt = poisson.stats(n, p, moments="mvsk")
-x = np.arange(poisson.ppf(0.01, mu), poisson.ppf(0.99, mu))
-ax.plot(x, poisson.pmf(x, mu), "bo", ms=8, label="poisson pmf")
-ax.vlines(x, 0, poisson.pmf(x, mu), colors="b", lw=5, alpha=0.5)
+mean = 0.5
 
 # a. What is the value of the parameter λ?
+# lambda is a reserved key for python so I cannot use it for a variable name
+mu = 1 / 0.5
+print(mu)
 
 # b. What is the median time between requests?
+print(expon.median(scale=1 / mu))
 
 # c. What is the standard deviation?
+print(expon.std(scale=1 / mu))
 
 # d. What is the 80th percentile?
+x = symbols("x")
+expr = Eq(1 - e ** (-mu * x) - 0.8, 0)
+x_80 = solve(expr)
+print(x_80[0])
 
 # e. Find the probability that more than one second elapses between requests.
+print(1 - expon.cdf(1, scale=1 / mu))
 
 # f. If there have been no requests for the past two seconds, what is the probability
 #    that more than one additional second will elapse before the next request?
+print(1 - expon.cdf(1, scale=1 / mu))
 
+x = np.linspace(expon.ppf(0.01, scale=1 / mu), expon.ppf(0.99, scale=1 / mu), 100)
+plt.plot(x, expon.pdf(x, scale=1 / mu))
+plt.plot(x, expon.cdf(x, scale=1 / mu))
 plt.show()
 
 ### Extra Credit Sec 4.10, #4 (10 points)
@@ -144,17 +168,87 @@ geyser Old Faithful in Yellowstone National Park.
 79 60 86 71 67 81 76 83 76 55 
 """
 # Setup
-mu = 10
-fig, ax = plt.subplots(1, 1)
-mean, var, skew, kurt = poisson.stats(n, p, moments="mvsk")
-x = np.arange(poisson.ppf(0.01, mu), poisson.ppf(0.99, mu))
-ax.plot(x, poisson.pmf(x, mu), "bo", ms=8, label="poisson pmf")
-ax.vlines(x, 0, poisson.pmf(x, mu), colors="b", lw=5, alpha=0.5)
+data = [
+    91,
+    51,
+    79,
+    53,
+    82,
+    51,
+    76,
+    82,
+    84,
+    53,
+    86,
+    51,
+    85,
+    45,
+    88,
+    51,
+    80,
+    49,
+    82,
+    75,
+    73,
+    67,
+    68,
+    86,
+    72,
+    75,
+    75,
+    66,
+    84,
+    70,
+    79,
+    60,
+    86,
+    71,
+    67,
+    81,
+    76,
+    83,
+    76,
+    55,
+]
 
-# a. Construct a normal probability plot for these data. 
-
-# b. Do they appear to come from an approximately normal distribution?
-
+# a. Construct a normal probability plot for these data.
+probplot(data, dist="norm", fit=True, plot=plt)
 plt.show()
 
+# b. Do they appear to come from an approximately normal distribution?
+# No, the data do not lie close to a straight line at all points. 
+
 ### OUTPUT
+# ------------------------------------------------------------------------
+# 1. Sec 4.5, #4 (15 points)
+# ------------------------------------------------------------------------
+# 0.5
+# 0.5827683075454216
+# 0.091848052662599
+# 0.6826894921370859
+# ------------------------------------------------------------------------
+# 2. Sec 4.5, #12 (15 points)
+# ------------------------------------------------------------------------
+# std 0.4989056210478824
+# mean 18.939373279712914
+# 0.9701408503200377
+# ------------------------------------------------------------------------
+# 3. Sec 4.5, #16 (15 points)
+# ------------------------------------------------------------------------
+# 0.7475074624530773
+# 6.38446546966338
+# 0.031326036760685176
+# ------------------------------------------------------------------------
+# 4. Sec 4.7, #2 (20 points)
+# ------------------------------------------------------------------------
+# 2.0
+# 0.34657359027997264
+# 0.5
+# 0.804718956217049
+# 0.1353352832366127
+# 0.1353352832366127
+# ------------------------------------------------------------------------
+# Extra Credit Sec 4.10, #4 (10 points)
+# ------------------------------------------------------------------------
+# See the plot
+# No, the data do not lie close to a straight line at all points. 
