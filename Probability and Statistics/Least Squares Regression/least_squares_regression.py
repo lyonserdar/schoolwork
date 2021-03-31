a780974993b5276f3525ba97db91284c341a1d0c
Serdar Lyon <lyonserdar@gmail.com>
2021-03-31 16:04:18 -0600

    ECE 3710 - Project v1

--- Probability and Statistics/Least Squares Regression/least_squares_regression.py
+++ Probability and Statistics/Least Squares Regression/least_squares_regression.py
@@ -13,22 +13,43 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 plt.style.use("ggplot")
 
 SEED = 1  # Set seed for reproducibility
 n = 50  # Sample size
 
 # Import data from the csv file
-df = pd.read_csv("sample_temperature_data.csv")
-# df = pd.read_csv("file.csv")  # Read the temp data from csv file
+# df = pd.read_csv("sample_temperature_data.csv")
+df = pd.read_csv("weather_data.csv")  # Read the temp data from csv file
+# df = df[["date", "temperature"]]
+df["date"] = pd.to_datetime(df["date"])
 # print(df.head())  # See first 5 elements of the dataframe
 # print(df.shape)  # Size of the dataframe
-df = df.reset_index()
-X = (df[["index"]] + 1).values
-y = df[["TEMP"]].values  # avg temperature
-# X_scaled = (X - X.mean()) / X.std() # Z
+# df = df.append(df, ignore_index=True)
+# df = df.append(df)
+
+# df["date"] = df["date"].str[:-3]
+# print(df.head())
+
+
+df["day"] = df.apply(lambda row: pd.Period(row.date, freq="D").dayofyear, axis=1)
+print(df.head())
+
+X = df[["day"]].values
+y = df[["temperature"]].values
+
+# Scale the X and y
 X_scaled = (X - min(X)) / (max(X) - min(X))  # 0 to 1
 y_scaled = (y - y.mean()) / y.std()
+
+
+# df = df.reset_index()
+# X = (df[["index"]] + 1).values
+# y = df[["TEMP"]].values  # avg temperature
+# X_scaled = (X - X.mean()) / X.std() # Z
+# X_scaled = (X - min(X)) / (max(X) - min(X))  # 0 to 1
+# y_scaled = (y - y.mean()) / y.std()
 
 # a. Compute the mean and variance of the population.
 # print(df.describe())
@@ -43,14 +64,11 @@ print(f"Mean and Variance of temp(y): {y_mean}, {y_variance}")
 # b. Use simple random sample (SRS) sampling method and randomly select 50 samples.
 # Then, compute the sample mean and sample variance.
 df_sample = df.sample(n=n, random_state=SEED)
-# print(df_sample.head())
-# print(df_sample.describe())
-df_sample = df_sample.reset_index()
 
-X_sample = (df_sample[["index"]] + 1).values
-y_sample = df_sample[["TEMP"]].values
+X_sample = df_sample[["day"]].values
+y_sample = df_sample[["temperature"]].values
 
-X_sample_scaled = (X_sample - X_sample.mean()) / X_sample.std()  # Z
+X_sample_scaled = (X_sample - min(X_sample)) / (max(X_sample) - min(X_sample))
 y_sample_scaled = (y_sample - y_sample.mean()) / y_sample.std()
 
 X_sample_mean = X_sample.mean()
@@ -95,12 +113,10 @@ plt.axhline(y_sample_mean, color="b", linestyle="--")
 plt.show()
 
 # Scaled
-plt.scatter(X_scaled, y)
+plt.scatter(X_scaled, y_scaled)
 plt.title("Temp Scaled")
 plt.xlabel("Day of the year (Scaled)")
-plt.ylabel("Temperature in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
+plt.ylabel("Temperature in F (Scaled)")
 plt.show()
 
 
@@ -133,79 +149,69 @@ plt.ylabel("Temperature in F")
 plt.legend()
 plt.show()
 
-plt.scatter(X_scaled, y)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year (Scaled)")
+b0, b1, b2 = 0, 0, 0
+
+
+# df_sample[""] = (df_sample[["index"]] + 1).values
+# df_sample[""] = (df_sample[["index"]] + 1).values
+
+
+X_matrix = np.column_stack((X_sample, np.ones(X_sample.size)))
+
+b = np.dot(
+    np.dot(
+        np.linalg.inv(np.dot(np.transpose(X_matrix), X_matrix)), np.transpose(X_matrix)
+    ),
+    y_sample,
+)
+
+print(b)
+
+X_matrix = np.column_stack((X_sample ** 2, X_sample, np.ones(X_sample.size)))
+
+b = np.dot(
+    np.dot(
+        np.linalg.inv(np.dot(np.transpose(X_matrix), X_matrix)), np.transpose(X_matrix)
+    ),
+    y_sample,
+)
+
+print(b)
+
+X_pred = np.linspace(np.min(X), np.max(X), 1000)
+y_pred = b[0] * X_pred ** 2 + b[1] * X_pred + b[2]
+
+# i. Overlay on the plot in step “f” the predictions of your regression model for each
+# day of the year.
+plt.scatter(X, y)
+plt.plot(X_pred, y_pred, c="g", label="Regression Line")
+plt.title("Temp with Regression Line")
+plt.xlabel("Day of the year (eg., 1, 2, ...)")
 plt.ylabel("Temperature in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
+plt.legend()
 plt.show()
-
-plt.scatter(X_scaled ** 2, y)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year x^2 (Scaled)")
-plt.ylabel("Temperature in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled ** 3, y)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year x^3 (Scaled)")
-plt.ylabel("Temperature in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled ** 10, y)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year x^10 (Scaled)")
-plt.ylabel("Temperature in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled, y)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year (Scaled)")
-plt.ylabel("Temperature in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled, y ** 2)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year (Scaled)")
-plt.ylabel("Temperature^2 in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled, y ** 3)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year (Scaled)")
-plt.ylabel("Temperature^3 in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled, y ** 10)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year (Scaled)")
-plt.ylabel("Temperature^10 in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
-plt.scatter(X_scaled ** 2, y ** 2)
-plt.title("Temp Scaled")
-plt.xlabel("Day of the year (Scaled)")
-plt.ylabel("Temperature^2 in F")
-plt.axhline(y_mean, color="r", linestyle="--")
-plt.axhline(y_sample_mean, color="b", linestyle="--")
-plt.show()
-
 
 # j. Using your regression model, make a prediction for the average temperature
 # on June 15, 2021.
+predict_date = "2021-06-15"
+predict_day = pd.Period(predict_date, freq="D").dayofyear  # Day 166
+prediction = b[0] * predict_day ** 2 + b[1] * predict_day + b[2]
+print(
+    f"Prediction for {predict_date} is {prediction} F."
+)  #  Predicted Temp: 67.77056534 F
 
+
+
+predict_date = "2021-12-15"
+predict_day = pd.Period(predict_date, freq="D").dayofyear  # Day 166
+prediction = b[0] * predict_day ** 2 + b[1] * predict_day + b[2]
+print(
+    f"Prediction for {predict_date} is {prediction} F."
+)  #  Predicted Temp: 67.77056534 F
+
+predict_date = "2021-1-15"
+predict_day = pd.Period(predict_date, freq="D").dayofyear  # Day 166
+prediction = b[0] * predict_day ** 2 + b[1] * predict_day + b[2]
+print(
+    f"Prediction for {predict_date} is {prediction} F."
+)  #  Predicted Temp: 67.77056534 F
