ffc44b44bed2acc5fcaa39cddf3c19098a8ccd4f
Serdar Lyon <lyonserdar@gmail.com>
2021-06-11 01:19:10 -0600

    CS 2420 - Project 6 completed

--- Intro to Algorithms and Data Structures/Project 6 - Dictionaries/hashmap.py
+++ Intro to Algorithms and Data Structures/Project 6 - Dictionaries/hashmap.py
@@ -5,7 +5,7 @@ Project: 6 - Hashmap
 Author: Ali Serdar Aydogdu
 Course: CS 2420-601
 Date Created: 06/10/2021
-Date Last Modified: 06/10/2021
+Date Last Modified: 06/11/2021
 """
 
 
@@ -18,55 +18,126 @@ class HashMap:
         """
         Initialize
         """
-        pass
+        self.__capacity = 7
+        self.__size = 0
+        self.__hash_map = [[] for _ in range(self.__capacity)]
 
-    def __get_hash(self, key):
+    def __hash(self, key):
         """
         Returns the hash value of the key
         """
-        pass
+        row, col = key
+        return (row * 10 + col) % self.__capacity
+
+    def __rehash(self):
+        self.__capacity = 2 * self.__capacity - 1
+        new_hash_map = [[] for _ in range(self.__capacity)]
+
+        for bucket in self.__hash_map:
+            for index, entry in enumerate(bucket):
+                entry_key, entry_value = entry
+                hash_key = self.__hash(entry_key)
+                new_hash_map[hash_key].append((entry))
+
+        self.__hash_map = new_hash_map
 
     def get(self, key):
         """
         Returns the value for key if key is in the dictionary.
         If key is not in the dictionary, raises a KeyError.
         """
-        pass
+        hash_key = self.__hash(key)
+        bucket = self.__hash_map[hash_key]
+
+        for index, entry in enumerate(bucket):
+            entry_key, entry_value = entry
+            if key == entry_key:
+                return entry_value
+
+        raise KeyError()
 
     def set(self, key, value):
         """
         Adds the key-value pair to the hashmap.
         If the load-factor >= 80%, rehashes the hashmap and doubles the capacity.
         """
-        pass
+        hash_key = self.__hash(key)
+        bucket = self.__hash_map[hash_key]
+
+        key_found = False
+
+        for index, entry in enumerate(bucket):
+            entry_key, entry_value = entry
+            if key == entry_key:
+                bucket[index] = (key, value)
+                key_found = True
+                break
+
+        if not key_found:
+            bucket.append((key, value))
+            self.__size += 1
+
+        if self.__size / self.__capacity >= 0.80:
+            self.__rehash()
 
     def remove(self, key):
         """
         Removes the key-value pair from the hashmap.
         If the key does not exist, nothing happens.
         """
-        pass
+        hash_key = self.__hash(key)
+        bucket = self.__hash_map[hash_key]
+
+        for index, entry in enumerate(bucket):
+            entry_key, entry_value = entry
+            if key == entry_key:
+                bucket.remove(entry)
 
     def clear(self):
         """
         Empties the hashmap
         """
-        pass
+        self.__capacity = 7
+        self.__size = 0
+        self.__hash_map = [[] for _ in range(self.__capacity)]
 
     def capacity(self):
         """
         Returns the capacity of the hashmap.
         """
-        pass
+        return self.__capacity
 
     def size(self):
         """
         Returns the number of key-value pairs in the hashmap.
         """
-        pass
+        return self.__size
 
     def keys(self):
         """
         Returns a list of all keys in the hashmap.
         """
-        pass
+        keys_list = []
+        for bucket in self.__hash_map:
+            for index, entry in enumerate(bucket):
+                entry_key, entry_value = entry
+                keys_list.append(entry_key)
+
+        return keys_list
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def __contains__(self, key):
+        hash_key = self.__hash(key)
+        bucket = self.__hash_map[hash_key]
+
+        for index, entry in enumerate(bucket):
+            entry_key, entry_value = entry
+            if key == entry_key:
+                return True
+
+        return False
--- Intro to Algorithms and Data Structures/Project 6 - Dictionaries/pyramid.py
+++ Intro to Algorithms and Data Structures/Project 6 - Dictionaries/pyramid.py
@@ -5,5 +5,57 @@ Project: 6 - Hashmap | Pyramid
 Author: Ali Serdar Aydogdu
 Course: CS 2420-601
 Date Created: 06/10/2021
-Date Last Modified: 06/10/2021
+Date Last Modified: 06/11/2021
 """
+
+import sys
+from time import perf_counter
+from hashmap import HashMap
+
+
+function_call_counter = 0
+cache_hits_counter = 0
+cache = HashMap()
+
+
+def weight_on(r, c):
+    weight = 0
+    global function_call_counter
+    global cache_hits_counter
+    function_call_counter += 1
+    if (r, c) in cache:
+        cache_hits_counter += 1
+        return cache[(r, c)]
+    if r < 0 or c < 0:
+        raise Exception("r and c must be greater than or equal to 0")
+    elif c > r:
+        raise Exception("c must be smaller than or equal to r")
+    elif r == 0:
+        weight = 0.0
+    elif c == 0:
+        weight = weight_on(r - 1, 0) / 2 + 100
+    elif c == r:
+        weight = weight_on(r - 1, c - 1) / 2 + 100
+    else:
+        weight = weight_on(r - 1, c - 1) / 2 + 100 + weight_on(r - 1, c) / 2 + 100
+    cache[(r, c)] = weight
+    return weight
+
+
+def main():
+    if len(sys.argv) > 1:
+        row_count = int(sys.argv[1])
+        for r in range(row_count):
+            for c in range(r + 1):
+                print(f"{weight_on(r, c):.2f}", end=" ")
+            print()
+
+
+if __name__ == "__main__":
+    start = perf_counter()
+    main()
+    end = perf_counter() - start
+    print()
+    print(f"Elapsed time: {end:.20f} seconds")
+    print("Number of function calls:", function_call_counter)
+    print("Number of cache hits:", cache_hits_counter)
